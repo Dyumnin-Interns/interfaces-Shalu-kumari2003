15bf7a0a67838a1398bed058821fef5efad042ab
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-06 04:30:10 +0530

    Update delayed_dut.v

--- hdl/delayed_dut.v
+++ hdl/delayed_dut.v
@@ -1,4 +1,4 @@
-module delayed_dut (
+module dut (
     input CLK,
     input RST_N,
     input [2:0] read_address,
@@ -10,28 +10,33 @@ module delayed_dut (
     input [0:0] write_data,
     input write_en
 );
-    // Same implementation as dut.v but with one cycle delay
     wire [0:0] a_status, b_status, y_status, y_output;
-    reg [0:0] a_data, b_data;
-    reg [0:0] delayed_output;
+    wire [0:0] a_data, b_data;
     
+    // Instantiate FIFOs
     FIFO1 a_fifo(CLK, RST_N, write_en && write_address==4, write_data, a_status, a_data, );
     FIFO1 b_fifo(CLK, RST_N, write_en && write_address==5, write_data, b_status, b_data, );
     FIFO2 y_fifo(CLK, RST_N, a_status && b_status, a_data ^ b_data, , y_output, y_status);
     
-    always @(posedge CLK) begin
-        delayed_output <= y_output;
-        read_rdy <= read_en;
-        write_rdy <= 1;
-        
-        if (read_en) begin
-            case (read_address)
-                0: read_data <= a_status;
-                1: read_data <= b_status;
-                2: read_data <= y_status;
-                3: read_data <= delayed_output;
-                default: read_data <= 0;
-            endcase
+    always @(posedge CLK or negedge RST_N) begin
+        if (!RST_N) begin
+            read_data <= 0;
+            read_rdy <= 0;
+            write_rdy <= 1;
+        end
+        else begin
+            read_rdy <= read_en;
+            write_rdy <= 1;
+            
+            if (read_en) begin
+                case (read_address)
+                    0: read_data <= a_status;
+                    1: read_data <= b_status;
+                    2: read_data <= y_status;
+                    3: read_data <= y_output;  // XOR output
+                    default: read_data <= 0;
+                endcase
+            end
         end
     end
 endmodule
