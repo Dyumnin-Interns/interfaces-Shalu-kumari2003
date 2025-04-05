2d5bcfa0128d81d100271a8a98c63a56b50c2e56
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-06 01:46:27 +0530

    Update dut.v

--- hdl/dut.v
+++ hdl/dut.v
@@ -1,52 +1,28 @@
-module dut (
-    input clk,
-    input reset_n,
-    input A_data,
-    input A_enable,
-    output A_ready,
-    input B_data,
-    input B_enable,
-    output B_ready,
-    output Y_data,
-    output Y_enable,
-    input Y_ready
-);
-    // Instantiate FIFO1 for input A
-    FIFO1 fifo_A (
-        .clk(clk),
-        .reset_n(reset_n),
-        .data_in(A_data),
-        .enable_in(A_enable),
-        .ready_out(A_ready),
-        .data_out(A_fifo_out),
-        .enable_out(A_fifo_enable),
-        .ready_in(A_fifo_ready)
-    );
+// BSV/mem_if.bsv
+package MemIfc;
 
-    // Instantiate FIFO1 for input B
-    FIFO1 fifo_B (
-        .clk(clk),
-        .reset_n(reset_n),
-        .data_in(B_data),
-        .enable_in(B_enable),
-        .ready_out(B_ready),
-        .data_out(B_fifo_out),
-        .enable_out(B_fifo_enable),
-        .ready_in(B_fifo_ready)
-    );
+interface MemIfc;
+    method Action put(Bit#(1) a, Bit#(1) b);
+    method Bit#(1) get();
+endinterface
 
-    // Instantiate delayed_dut (XOR Logic)
-    delayed_dut xor_logic (
-        .clk(clk),
-        .reset_n(reset_n),
-        .A_data(A_fifo_out),
-        .A_enable(A_fifo_enable),
-        .A_ready(A_fifo_ready),
-        .B_data(B_fifo_out),
-        .B_enable(B_fifo_enable),
-        .B_ready(B_fifo_ready),
-        .Y_data(Y_data),
-        .Y_enable(Y_enable),
-        .Y_ready(Y_ready)
-    );
+module mkXOR(MemIfc);
+    Reg#(Bit#(1)) x <- mkReg(0);
+    Reg#(Bit#(1)) y <- mkReg(0);
+    Reg#(Bit#(1)) out <- mkReg(0);
+
+    rule compute;
+        out <= x ^ y;
+    endrule
+
+    method Action put(Bit#(1) a, Bit#(1) b);
+        x <= a;
+        y <= b;
+    endmethod
+
+    method Bit#(1) get();
+        return out;
+    endmethod
 endmodule
+
+endpackage
