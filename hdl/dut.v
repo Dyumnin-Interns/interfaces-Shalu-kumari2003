43f2c9d4e2c80c12b15b426ac98014053c87a751
shalu <127283835+Shalu-kumari2003@users.noreply.github.com>
2025-04-06 01:54:25 +0530

    Update dut.v

--- hdl/dut.v
+++ hdl/dut.v
@@ -1,28 +1,12 @@
-// BSV/mem_if.bsv
-package MemIfc;
-
-interface MemIfc;
-    method Action put(Bit#(1) a, Bit#(1) b);
-    method Bit#(1) get();
-endinterface
-
-module mkXOR(MemIfc);
-    Reg#(Bit#(1)) x <- mkReg(0);
-    Reg#(Bit#(1)) y <- mkReg(0);
-    Reg#(Bit#(1)) out <- mkReg(0);
-
-    rule compute;
-        out <= x ^ y;
-    endrule
-
-    method Action put(Bit#(1) a, Bit#(1) b);
-        x <= a;
-        y <= b;
-    endmethod
-
-    method Bit#(1) get();
-        return out;
-    endmethod
+module dut(
+    input clk,
+    input rst,
+    input a,
+    input b,
+    output reg out
+);
+always @(posedge clk or posedge rst) begin
+    if (rst) out <= 1'b0;
+    else out <= a ^ b;
+end
 endmodule
-
-endpackage
